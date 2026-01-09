2a6730b0236d004ab640faf9f1f9b2a3ddda3f53
sumitlalasangi <159750705+sumitlalasangi@users.noreply.github.com>
2026-01-09 14:41:24 +0530

    Add workspace 2Xy1tdpD565hXG1Isf8e files from XenAi

---
+++ test.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// This class represents a simple HelloWorld program.
+
+// The main method is the entry point of the program.
+// It takes an array of String arguments, which can be used to pass command-line arguments to the program.
+
+// This line prints a message to the standard output stream.
+// It uses the println method of the System.out object to display the string "Hello, World!" on the console.
